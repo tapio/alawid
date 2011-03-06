80911ac8208e00222ec45bde2453022ad8f59f1d
Tapio Vierros <tapiovierros@gmail.com>
2011-03-06 20:30:27 +0200

    Added normal map for wall texture.

--- world.js
+++ world.js
@@ -214,19 +214,21 @@ function World() {
 	this.draw = function() {
 		gl.uniform1i(curProg.textureSamplerUniform, 0);
 		gl.uniform1i(curProg.normalMapSamplerUniform, 1);
-		// Floor
 		gl.uniform1i(curProg.enableNormalMapUniform, 1);
+		// Floor
 		gl.uniform1f(curProg.materialShininessUniform, NO_SPECULAR);
 		gl.activeTexture(gl.TEXTURE0);
 		gl.bindTexture(gl.TEXTURE_2D, textures["floor"]);
 		gl.activeTexture(gl.TEXTURE1);
 		gl.bindTexture(gl.TEXTURE_2D, textures["floor_normalmap"]);
 		this.floorBuffer.draw();
-		gl.uniform1i(curProg.enableNormalMapUniform, 0);
 		// Walls
 		gl.uniform1f(curProg.materialShininessUniform, 32.0);
 		gl.activeTexture(gl.TEXTURE0);
 		gl.bindTexture(gl.TEXTURE_2D, textures["wall"]);
+		gl.activeTexture(gl.TEXTURE1);
+		gl.bindTexture(gl.TEXTURE_2D, textures["wall_normalmap"]);
 		this.wallBuffer.draw();
+		gl.uniform1i(curProg.enableNormalMapUniform, 0);
 	}
 }
