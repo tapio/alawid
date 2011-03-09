fe24340924855509a34d8d0b24f3ddeea1ae4edb
Tapio Vierros <tapiovierros@gmail.com>
2011-03-09 18:42:21 +0200

    Added $-function for easy HTML element access.

--- game/webgl-core.js
+++ game/webgl-core.js
@@ -317,3 +317,16 @@ function matchPos(p1, p2) {
 	return Math.round(p1[0]) == Math.round(p2[0])
 		&& Math.round(p1[1]) == Math.round(p2[1]);
 }
+
+function $() {
+	var elements = new Array();
+	for (var i = 0; i < arguments.length; ++i) {
+		var element = arguments[i];
+		if (typeof element == 'string')
+			element = document.getElementById(element);
+		if (arguments.length == 1)
+			return element;
+		elements.push(element);
+	}
+	return elements;
+}
